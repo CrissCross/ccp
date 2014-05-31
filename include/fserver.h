79cf3f098336103855f447f86c5bc5dcc69070f0
anticris <cristoffel@durga.ch>
2014-05-31 18:50:26 +0200

    fserver.h with global vars

--- include/fserver.h
+++ include/fserver.h
@@ -1,2 +1,8 @@
-int read_file ();
-int create_shm();
+// Global variables
+#define F_LIMIT 256 // max number of files
+#define F_MAX_LEN 100 // max length of a file name
+
+struct file_supervisor
+{
+  char files[F_LIMIT][F_MAX_LEN];
+};
