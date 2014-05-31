71b9129c2eac0d6bf18dc6a9fbfc44fb8ce7b74d
anticris <cristoffel@durga.ch>
2014-05-31 18:51:55 +0200

    added error handler for my own functions with no errno set

--- include/helpers.h
+++ include/helpers.h
@@ -2,6 +2,8 @@ enum exit_type { PROCESS_EXIT, THREAD_EXIT, NO_EXIT };
 /* helper function for dealing with errors */
 int handle_error(long return_code, const char *msg, enum exit_type et);
 
+int handle_my_error(long return_code, const char *msg, enum exit_type et);
+
 void handle_error_myerrno(long return_code, int myerrno, const char *msg, enum exit_type et);
 
 void handle_ptr_error(void *ptr, const char *msg, enum exit_type et);
--- lib/error_handler.c
+++ lib/error_handler.c
@@ -20,11 +20,12 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-#include <helpers.h>
+#include "helpers.h"
 
 #define ERROR_SIZE 4096
 
-void exit_by_type(enum exit_type et, int myerrno) {
+void exit_by_type(enum exit_type et, int myerrno)
+{
   switch (et) {
   case PROCESS_EXIT: 
     exit(myerrno);
@@ -33,7 +34,7 @@ void exit_by_type(enum exit_type et, int myerrno) {
     pthread_exit(NULL);
     break;
   case NO_EXIT:
-    printf("continuing\n");
+    //printf("continuing\n");
     break;
   default:
     printf("unknown exit_type=%d\n", et);
@@ -44,7 +45,8 @@ void exit_by_type(enum exit_type et, int myerrno) {
 
 
 /* helper function for dealing with errors */
-void handle_error_myerrno(long return_code, int myerrno, const char *msg, enum exit_type et) {
+void handle_error_myerrno(long return_code, int myerrno, const char *msg, enum exit_type et)
+{
   if (return_code < 0) {
     char extra_msg[ERROR_SIZE];
     char error_msg[ERROR_SIZE];
@@ -55,24 +57,51 @@ void handle_error_myerrno(long return_code, int myerrno, const char *msg, enum e
       extra_msg[0] = '\000';
     }
     sprintf(error_msg, "%sreturn_code=%ld\nerrno=%d\nmessage=%s\n", extra_msg, return_code, myerrno, error_str);
-    write(STDOUT_FILENO, error_msg, strlen(error_msg));
+    write(STDERR_FILENO, error_msg, strlen(error_msg));
     exit_by_type(et, myerrno);
   }
 }
 
-void handle_thread_error(int retcode, const char *msg, enum exit_type et) {
+void handle_thread_error(int retcode, const char *msg, enum exit_type et)
+{
   if (retcode != 0) {
     handle_error_myerrno(-abs(retcode), retcode, msg, et);
   }
 }
 
 /* helper function for dealing with errors */
-int handle_error(long return_code, const char *msg, enum exit_type et) {
+int handle_error(long return_code, const char *msg, enum exit_type et)
+{
   if (return_code < 0)
   { // do error handling
     int myerrno = errno;
     handle_error_myerrno(return_code, myerrno, msg, et);
-    return 1;
+    return -1;
+  }
+  else
+  { // no error occured
+    return 0;
+  }
+}
+
+/* helper function for my own errors, no errno set*/
+int handle_my_error(long return_code, const char *msg, enum exit_type et) 
+{
+
+  if (return_code < 0)
+  { // do error handling
+    char extra_msg[ERROR_SIZE];
+    char error_msg[ERROR_SIZE];
+    if (msg != NULL) {
+      sprintf(extra_msg, "%s\n", msg);
+    } else {
+      extra_msg[0] = '\000';
+    }
+    sprintf(error_msg, "%sreturn_code=%ld\n", extra_msg, return_code);
+    write(STDERR_FILENO, error_msg, strlen(error_msg));
+    exit_by_type(et, return_code);
+
+    return -1;
   }
   else
   { // no error occured
@@ -81,14 +110,16 @@ int handle_error(long return_code, const char *msg, enum exit_type et) {
 }
 
 
-void handle_ptr_error(void *ptr, const char *msg, enum exit_type et) {
+void handle_ptr_error(void *ptr, const char *msg, enum exit_type et)
+{
   if (ptr == NULL) {
     handle_error(-1L, msg, et);
   }
 }
 
 
-void die_with_error(char *error_message) {
+void die_with_error(char *error_message)
+{
     perror(error_message);
     exit(1);
 }
