c5470171c0a23f1119467ca586ae0b6870e6ed4c
anticris <cristoffel@durga.ch>
2014-06-07 13:35:08 +0200

    modified handle_ptr_error

--- include/helpers.h
+++ include/helpers.h
@@ -6,7 +6,7 @@ int handle_my_error(long return_code, const char *msg, enum exit_type et);
 
 void handle_error_myerrno(long return_code, int myerrno, const char *msg, enum exit_type et);
 
-void handle_ptr_error(void *ptr, const char *msg, enum exit_type et);
+int handle_ptr_error(void *ptr, const char *msg, enum exit_type et);
 
 void die_with_error(char *error_message);
 
